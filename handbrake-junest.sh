af1e41b1599e29049553115fe5bc072ed6e8a9ad
iVAN <88724353+ivan-hc@users.noreply.github.com>
2023-07-16 19:19:14 +0200

    Save only locales for handbrake

--- handbrake-junest.sh
+++ handbrake-junest.sh
@@ -86,11 +86,14 @@ sed -i 's/rm -f "${JUNEST_HOME}${bin_path}_wrappers/#rm -f "${JUNEST_HOME}${bin_
 sed -i 's/ln/#ln/g' ./$APP.AppDir/.local/share/junest/lib/core/wrappers.sh
 
 # REMOVE SOME BLOATWARES, ADD HERE ALL THE FOLDERS THAT YOU DON'T NEED FOR THE FINAL APPIMAGE
+find ./$APP.AppDir/.junest/usr/share/locale/*/*/* -not -iname "*$BIN*" -a -not -name "." -delete
+
 rm -R -f ./$APP.AppDir/.junest/var/cache/pacman/pkg/*
 
 mkdir save
 cp -r ./$APP.AppDir/.junest/usr/share/gdb ./save/
 cp -r ./$APP.AppDir/.junest/usr/share/*gst* ./save/
+cp -r ./$APP.AppDir/.junest/usr/share/locale ./save/
 rm -R -f ./$APP.AppDir/.junest/usr/share/*
 mv ./save/* ./$APP.AppDir/.junest/usr/share/
 
